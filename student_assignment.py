db660b561423d6704ff4c1b3c78e693441acb61a
jeremyys_chen <jeremyys_chen@asus.com>
2025-03-06 17:27:08 +0800

    hw02_2 ver.1

--- student_assignment.py
+++ student_assignment.py
@@ -5,8 +5,8 @@ q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
 
 
-def hw02_1(q1_pdf):
-    loader = PyPDFLoader(q1_pdf)
+def hw02_1(pdf_file):
+    loader = PyPDFLoader(pdf_file)
     documents = loader.load()
     print(documents)
 
@@ -21,12 +21,28 @@ def hw02_1(q1_pdf):
     return chunks[-1]
 
 
-def hw02_2(q2_pdf):
-    pass
+def hw02_2(pdf_file):
+    loader = PyPDFLoader(pdf_file)
+    documents = loader.load()
+    full_text = "\n".join(doc.page_content for doc in documents)
+    print(full_text)
+
+    text_splitter = CharacterTextSplitter(
+        separator=r"(第\s*.+\s*[條章].+\n)",
+        chunk_size=10,  # 設置較大值以容納完整條文
+        chunk_overlap=0,  # 無重疊
+        is_separator_regex=True  # 啟用正則表達式
+    )
+
+    chunks = text_splitter.split_text(full_text)
+    print(chunks)
+    print(len(chunks))
+    return len(chunks)
 
 
 def main():
-    hw02_1(q1_pdf)
+    # hw02_1(q1_pdf)
+    hw02_2(q2_pdf)
 
 
 if __name__ == '__main__':
